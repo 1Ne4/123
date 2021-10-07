41539847e9f6e1cf088405ae1e84bb5ca1a6bcbb
TheHomeGera <suchkovgera12@gmail.com>
2021-10-07 13:43:19 +0500

    Добавил Стамину

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -12,8 +12,8 @@ public class PlayerController : MonoBehaviour
     private bool OnGround = false;
     public float Stamina = 100;
 
-    public float speed = 2;
-    public float jumpSpeed = 500;
+    private float NormalSpeed = 3, FastedSpeed = 5, currentSpeed = 3;
+    private float jumpSpeed = 150;
     public GameObject Camera;
     public float sensivity = 1;
 
@@ -23,12 +23,14 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         MoveCharacter();
         MoveCamera();
-        if (Stamina < 100)
+        if (Stamina < 100 && Stamina > 10)
             Stamina += 0.05f;
+        else if (Stamina < 10)
+            Stamina += 0.025f;
     }
 
     private void OnCollisionStay(Collision collision)
@@ -44,11 +46,26 @@ public class PlayerController : MonoBehaviour
 
     private void MoveCharacter()
     {
+        if (Stamina < 5)
+            currentSpeed = 2;
+        if (Stamina > 2)
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                if (Stamina < 5)
+                    currentSpeed = 2;
+                else
+                    currentSpeed = FastedSpeed;
+                if (Stamina > 5)
+                    Stamina -= 0.07f;
+            }
+            else
+                currentSpeed = NormalSpeed;
+
         if (OnGround)
         {
-            Vertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
-            Horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-            if (Input.GetKeyDown(KeyCode.Space) && Stamina > 50) 
+            Vertical = Input.GetAxis("Vertical") * Time.deltaTime * currentSpeed;
+            Horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * currentSpeed;
+            if (Input.GetKey(KeyCode.Space) && Stamina > 50) 
             {
                 GetComponent<Rigidbody>().AddForce(0, jumpSpeed, 0);
                 Stamina -= 30;
